f2ea3e2672cee689f4d468b01dfd000457e2590c
Pankaj Gupta <74145586+PankajCre04@users.noreply.github.com>
2022-07-17 19:48:45 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,34 @@
+// Imports from Packages
+const express = require('express'); // Like Importing an package in flutter 
+const mongoose = require("mongoose");
+const adminRouter = require('./routes/admin');
+// INIT
+const app = express(); // Initializing express
+const PORT = process.env.PORT || 3000;
+const DB ="mongodb+srv://pankaj:pankaj1904@cluster0.47odx.mongodb.net/?retryWrites=true&w=majority";
+
+// Imports from other files
+const authRouter = require("./routes/auth");
+const productRouter = require('./routes/product');
+const userRouter = require('./routes/user');
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+
+
+// Connections np
+mongoose.connect(DB).then( () => {
+    console.log("Connection Successful");
+} ).catch( (e)=>{
+    console.log(e);
+} );
+;
+
+app.listen(PORT,"0.0.0.0",function (){
+    console.log(`connected port ${PORT}`);
+} );
+
+

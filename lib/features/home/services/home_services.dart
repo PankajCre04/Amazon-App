0e017005b49f0001ae78607c25ff2731a4115775
Pankaj Gupta <74145586+PankajCre04@users.noreply.github.com>
2022-07-21 15:24:23 +0530

    Update home_services.dart

--- lib/features/home/services/home_services.dart
+++ lib/features/home/services/home_services.dart
@@ -17,7 +17,7 @@ class HomeServices {
     List<Product> productList = [];
     try {
       http.Response res = await http.get(
-        Uri.parse("$uri/api/products?category=$category"),
+        Uri.parse("${uri}api/products?category=$category"),
         headers: {
           'Content-Type': 'application/json; charset=UTF-8',
           'x-auth-token': userProider.user.token
@@ -57,7 +57,7 @@ class HomeServices {
     );
     try {
       http.Response res = await http.get(
-        Uri.parse("$uri/api/deal-of-day"),
+        Uri.parse("${uri}api/deal-of-day"),
         headers: {
           'Content-Type': 'application/json; charset=UTF-8',
           'x-auth-token': userProider.user.token
